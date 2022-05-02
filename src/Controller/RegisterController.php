2d1eeca5678627e56730de0ed8fbf74a876bc550
Matthieu THOLOT-DECHENE <mtholot19@gmail.com>
2022-05-02 21:00:50 +0200

    Add login and register

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+class RegisterController extends AbstractController
+{
+    #[Route('/register', name: 'register')]
+        public function index(UserPasswordHasherInterface $passwordHasher, Request $request, EntityManagerInterface $manager)
+    {
+        // ... e.g. get the user data from a registration form
+
+
+        $user = new User();
+
+       $coco  = $request->getContent();
+
+       $coco = json_decode($coco, true);
+
+        $user->setEmail($coco["email"]);
+        $plaintextPassword = $coco["password"];
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+       // dd($user);
+
+        $manager->persist($user);
+        $manager->flush();
+
+        return new Response("ok");
+    }
+
+}
