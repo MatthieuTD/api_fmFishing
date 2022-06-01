d84da9bd23fe4b93421776e714be51fdc58b9581
Matthieu THOLOT-DECHENE <mtholot19@gmail.com>
2022-06-01 23:34:57 +0200

    Update User entity for Tostring()

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -46,7 +46,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
     public function __toString (): string
     {
-        return $this->getName();
+        return $this->getEmail();
     }
     public function getId(): ?int
     {
